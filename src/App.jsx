6c776d4498b95273a28b463f66fbc83091c10b37
NURiFront <toktogulovanurzanat@gmail.com>
2024-01-24 12:01:34 +0600

    added render to github

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { useEffect } from "react";
 import { useState } from "react";
+import { useEffect } from "react";
+// import { useState } from "react";
 
 const App = () => {
-	const [userId, setUserId] = useState(1);
-  console.log(userId);
+	const [posts, setPosts] = useState([]);
+	console.log(posts);
+	// const [userId, setUserId] = useState(1);
+	// console.log(posts);
 	const getTodos = async () => {
 		try {
 			const response = await fetch(
-				`https://jsonplaceholder.typicode.com/todos/${userId}`
+				`https://jsonplaceholder.typicode.com/posts`
 			);
 			const responseData = await response.json();
-			console.log(responseData);
+			setPosts(responseData);
 		} catch (error) {
 			console.error("Backend упал ", error);
 		}
@@ -18,16 +21,16 @@ const App = () => {
 
 	useEffect(() => {
 		getTodos();
-	}, [userId]);
+	}, []);
 
 	return (
 		<div>
-			<input
-				type="number"
-				value={userId}
-				onChange={(e) => setUserId(e.target.value)}
-			/>
-			{/* <button onClick={getTodos}>getTodos</button> */}
+			{posts.map((item) => (
+				<div key={item.id}>
+					<h1>{item.title}</h1>
+					<p>{item.body}</p>
+				</div>
+			))}
 		</div>
 	);
 };
